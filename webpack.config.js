3c7b910b68b894fe3cbbf14ff9ca5fbb13344366
roystiven <roy_stiven@hotmail.com>
2023-11-17 22:02:34 -0500

    asdsa

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "https://cozy-cajeta-685a1f.netlify.app",
+    publicPath: "/",
   },
 
   resolve: {
